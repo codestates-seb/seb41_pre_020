eaa799e9dff51473a091e80e24b485e5a5b51fce
kimmj13 <110887976+kimmj13@users.noreply.github.com>
2022-12-21 19:25:12 +0900

    [BE] 회원 기능 추가 (#18)
    * 공통기능 추가 - dto, exception, audit
    
    * :sparkles: feat: 회원 컨트롤러 추가
    
    회원 등록, 회원 수정, 회원(1인/전체) 조회, 회원 삭제
    
    * :sparkles: feat: 회원 리포지토리 추가
    
    * ✨feat: 회원 가입, 수정, 삭제, 조회 기능 추가
    
    * apply .gitignore

---
+++ server/stackoverflow/src/main/java/com/y2k/stackoverflow/member/dto/MemberDto.java
@@ -0,0 +1,48 @@
+package com.y2k.stackoverflow.member.dto;
+
+import com.y2k.stackoverflow.dto.PageInfo;
+import com.y2k.stackoverflow.member.entity.Member;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.domain.Page;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+import java.util.List;
+
+public class MemberDto {
+
+    @Getter @AllArgsConstructor
+    public static class Post {
+        @NotBlank
+        @Pattern(regexp = "^\\S+(\\s?\\S+)*$", message = "이름은 공백이 아니어야 합니다.")
+        private String displayName;
+
+        @NotBlank
+        @Email
+        private String email;
+
+        @NotBlank
+        @Pattern(regexp="(?=.*[0-9])(?=.*[a-z])(?=.*\\W)(?=\\S+$).{8,16}",
+        message = "비밀번호는 영문 대,소문자, 숫자, 특수기호를 포함한 8~16자리 이내로 구성되어야 합니다.")
+        private String password;
+    }
+
+    @Getter @Setter
+    @AllArgsConstructor
+    public static class Patch {
+        private long memberId;
+
+        @Pattern(regexp = "^\\S+(\\s?\\S+)*$", message = "이름은 공백이 아니어야 합니다.")
+        private String displayName;
+    }
+
+    @Getter @AllArgsConstructor
+    public static class Response {
+        private String displayName;
+        private String email;
+        private Member.MemberStatus memberStatus;
+    }
+}
