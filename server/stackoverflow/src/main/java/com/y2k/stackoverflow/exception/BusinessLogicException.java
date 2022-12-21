f91519497900cf5dcf619b0e38ea702a62f1ecdb
kimmj13 <110887976+kimmj13@users.noreply.github.com>
2022-12-21 14:37:33 +0900

    공통기능 추가 - dto, exception, audit (#11)

---
+++ server/stackoverflow/src/main/java/com/y2k/stackoverflow/exception/BusinessLogicException.java
@@ -0,0 +1,13 @@
+package com.y2k.stackoverflow.exception;
+
+import lombok.Getter;
+
+public class BusinessLogicException extends RuntimeException {
+    @Getter
+    private ExceptionCode exceptionCode;
+
+    public BusinessLogicException(ExceptionCode exceptionCode) {
+        super(exceptionCode.getMessage());
+        this.exceptionCode = exceptionCode;
+    }
+}
