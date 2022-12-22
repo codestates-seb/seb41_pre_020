d5edb9115bfb021314525801bd3a503d40ddd120
kim hyejin <97475429+hyejinme@users.noreply.github.com>
2022-12-22 14:06:14 +0900

    [BE] 공통기능 변경 (#24)
    * ✨feat: 질문 관련 엔티티, dto 추가
    
    * ✨feat: 질문 등록, 수정, 삭제, 조회 기능 추가
    
    * ✨feat: 공통 기능 변경

--- server/stackoverflow/src/main/java/com/y2k/stackoverflow/exception/ExceptionCode.java
+++ server/stackoverflow/src/main/java/com/y2k/stackoverflow/exception/ExceptionCode.java
@@ -7,6 +7,8 @@ public enum ExceptionCode {
 
     INVALID_MEMBER_STATUS(400, "Invalid member status"),
     COMMENT_NOT_FOUND(404, "Comment Not Found"),
+    ANSWER_NOT_FOUND(404, "Answer Not Found"),
+    QUESTION_NOT_FOUND(404, "Question Not Found"),
     MEMBER_NOT_FOUND(404, "Member not found"),
     METHOD_NOT_ALLOWED(405, "Method Not Allowed"),
     MEMBER_EXISTS(409, "Member exists"),
