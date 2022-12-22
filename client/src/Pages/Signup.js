277a79a9d5094872f59e84d74d5e24518ff123c5
jingoworld <lonul83@gmail.com>
2022-12-22 23:20:49 +0900

    ✨feat: signup 페이지 css 적용

--- client/src/Pages/Signup.js
+++ client/src/Pages/Signup.js
@@ -1,23 +1,38 @@
 import SignupText from '../components/signup/SignupText';
+import './Signup.css';
 
 function Signup() {
+  const signupHandler = (event) => {
+    event.preventDefault();
+  };
+
   return (
-    <div>
+    <form onSubmit={signupHandler}>
       <SignupText />
-      <div>
-        <span>Display name</span>
-        <input></input>
+      <div className='signupInputWrap'>
+        <div className='signupInputBox'>
+          <div className='signupForm'>
+            <div className='signupItem'>
+              <label htmlFor='username'>Display name</label>
+              <input id='username' type='text'></input>
+            </div>
+            <div className='signupItem'>
+              <label htmlFor='email'>Email</label>
+              <input id='email' type='email'></input>
+            </div>
+            <div className='signupItem'>
+              <label htmlFor='password'>Password</label>
+              <input id='password' type='text'></input>
+            </div>
+          </div>
+          <button type='submit'>Sign up</button>
+        </div>
+        <div className='signupInputLogin'>
+          <span>Already have an account?</span>
+          <span>Log in</span>
+        </div>
       </div>
-      <div>
-        <span>Email</span>
-        <input></input>
-      </div>
-      <div>
-        <span>Password</span>
-        <input></input>
-      </div>
-      <button type='submit'>Sign up</button>
-    </div>
+    </form>
   );
 }
 
--- client/src/components/signup/SignupText.js
+++ client/src/components/signup/SignupText.js
@@ -2,35 +2,39 @@ import { ReactComponent as SpeechBubble } from '../../image/SpeechBubbleQuestion
 import { ReactComponent as ArrowUpDown } from '../../image/ArrowUpDown.svg';
 import { ReactComponent as Tags } from '../../image/Tags.svg';
 import { ReactComponent as Achievements } from '../../image/Achievements.svg';
+import './SignupText.css';
 
 const SignupText = () => {
   return (
     <>
-      <div>
-        <div>Join the Stack Overflow community</div>
+      <div className='signupInfoWrap'>
+        <h1>Join the Stack Overflow community</h1>
 
-        <div>
-          <SpeechBubble />
-          <div>Get unstuck — ask a question</div>
-        </div>
-        <div>
-          <ArrowUpDown />
-          <div>Unlock new privileges like voting and commenting</div>
-        </div>
-        <div>
-          <Tags />
-          <div>Save your favorite tags, filters, and jobs</div>
-        </div>
-        <div>
-          <Achievements />
-          <div>Earn reputation and badges</div>
+        <div className='signupInfoItem'>
+          <SpeechBubble className='signupIcon' />
+          <span>Get unstuck — ask a question</span>
         </div>
 
-        <div>
+        <div className='signupInfoItem'>
+          <ArrowUpDown className='signupIcon' />
+          <span>Unlock new privileges like voting and commenting</span>
+        </div>
+
+        <div className='signupInfoItem'>
+          <Tags className='signupIcon' />
+          <span>Save your favorite tags, filters, and jobs</span>
+        </div>
+
+        <div className='signupInfoItem'>
+          <Achievements className='signupIcon' />
+          <span>Earn reputation and badges</span>
+        </div>
+
+        <p>
           Collaborate and share knowledge with a private group for FREE.
           <br></br>
           Get Stack Overflow for Teams free for up to 50 users.
-        </div>
+        </p>
       </div>
     </>
   );
