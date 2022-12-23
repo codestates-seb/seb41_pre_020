8b8a841ee661916116117c6c50566644c3ab6064
kim hyejin <97475429+hyejinme@users.noreply.github.com>
2022-12-23 17:13:46 +0900

    [BE] 질문 기능 수정, 추가 (#32)
    * ✨feat: 질문 관련 엔티티, dto 추가
    
    * ✨feat: 질문 등록, 수정, 삭제, 조회 기능 추가
    
    * ✨feat: 공통 기능 변경
    
    * ✨feat: 질문 Dto 유효성 검증 추가
    
    * etc: Tag entity 삭제 - questionTag 통합
    
    * ✨feat: 질문 조회수 기능 추가, 예외처리
    
    * ✨feat: 질문 조회 시 답변 함께 출력하는 기능 추가

--- server/stackoverflow/src/main/java/com/y2k/stackoverflow/question/dto/QuestionAnswerResponseDto.java
+++ server/stackoverflow/src/main/java/com/y2k/stackoverflow/question/dto/QuestionAnswerResponseDto.java
@@ -1,16 +1,19 @@
 package com.y2k.stackoverflow.question.dto;
 
-import lombok.*;
+import com.y2k.stackoverflow.answer.dto.AnswerResponseDto;
+import com.y2k.stackoverflow.answer.dto.AnswersGetResponseDto;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
-@Builder
 @Getter
 @Setter
-@NoArgsConstructor
-@AllArgsConstructor
-public class QuestionResponseDto {
+/**
+ * 질문을 눌렀을 때 그 질문에 대한 답변까지 함께 출력하기 위한 Dto
+ */
+public class QuestionAnswerResponseDto {
     private Long questionId;
     private String title;
     private String content;
@@ -19,4 +22,5 @@ public class QuestionResponseDto {
     private LocalDateTime lastModifiedAt;
     private int votes;
     private int views;
+    private AnswersGetResponseDto<AnswerResponseDto> answers;
 }
