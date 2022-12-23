88783cd461accc411aafda11c286e4d4ac52f94c
hyejinme <kimhyejin703@daum.net>
2022-12-23 16:16:12 +0900

    ✨feat: 질문 Dto 유효성 검증 추가

--- server/stackoverflow/src/main/java/com/y2k/stackoverflow/question/dto/QuestionResponseDto.java
+++ server/stackoverflow/src/main/java/com/y2k/stackoverflow/question/dto/QuestionResponseDto.java
@@ -11,13 +11,12 @@ import java.util.List;
 @NoArgsConstructor
 @AllArgsConstructor
 public class QuestionResponseDto {
-    //private Long memberId;
     private Long questionId;
     private String title;
     private String content;
     private List<QuestionTagResponseDto> questionTags;
-    private LocalDateTime createdAt = LocalDateTime.now();
-    private LocalDateTime lastModifiedAt = LocalDateTime.now();
-    private int votes = 0;
-    private int views = 0;
+    private LocalDateTime createdAt;
+    private LocalDateTime lastModifiedAt;
+    private int votes;
+    private int views;
 }
