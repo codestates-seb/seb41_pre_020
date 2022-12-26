04610494cee7db74e55a6950cd22d0d1ad8ddbdc
ashleysyheo <ashleysyheo@gmail.com>
2022-12-27 01:25:59 +0900

    fix: question page main section css

--- client/src/Pages/Home.js
+++ client/src/Pages/Home.js
@@ -1,35 +1,22 @@
-import SidebarLeft from "../components/aside/SidebarLeft";
-import SidebarRight from '../components/aside/SidebarRight';
-import styled from "styled-components";
-import Footer from "../components/Footer";
-// import List from "../components/home/List";
-import Main from "../components/home/Main";
-
-const HomeContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-`;
-
-const MainContainer = styled.div`
-  margin: 0 auto;
-  display: flex;
-`;
+import SidebarLeft from '../components/aside/SidebarLeft';
+import styled from 'styled-components';
+import Footer from '../components/Footer';
+import Main from '../components/home/Main';
+import { BodyContainer } from '../components/BodyContainer';
 
 const FooterContainer = styled.div``;
 
 const Home = () => {
   return (
-    <HomeContainer>
-        <MainContainer>
-            <SidebarLeft />
-            {/*<List />*/}
-            <Main />
-             {/*<SidebarRight />*/}
-        </MainContainer>
-        <FooterContainer>
-            <Footer />
-        </FooterContainer>
-    </HomeContainer>
+    <main>
+      <BodyContainer>
+        <SidebarLeft />
+        <Main />
+      </BodyContainer>
+      <FooterContainer>
+        <Footer />
+      </FooterContainer>
+    </main>
   );
 };
 
--- client/src/components/home/Main.js
+++ client/src/components/home/Main.js
@@ -6,8 +6,8 @@ import SidebarRight from '../aside/SidebarRight';
 
 const Content = styled.div`
   max-width: 1100px;
-  /* margin: 0 auto; */
-  /* width: 100%; */
+  margin: 0 auto;
+  width: 100%;
   height: 100% !important;
   width: calc(100% - 164px);
   background-color: white;
@@ -16,8 +16,8 @@ const Content = styled.div`
 `;
 
 const Mainbar = styled.div`
-  //width: 100%;
-  //width: calc(100% - 300px - 24px);
+  width: 100%;
+  width: calc(100% - 300px - 24px);
   float: left;
   margin: 0;
   padding: 0;
@@ -48,7 +48,7 @@ const Wrapper = styled.div`
     display: flex;
     flex-flow: row nowrap !important;
     margin-bottom: 1px;
-    border: 1px solid #838C95;
+    border: 1px solid #838c95;
     border-radius: 3px;
   }
 `;
@@ -69,11 +69,11 @@ const AskButton = styled.div`
 `;
 
 const SortButton = styled.div`
-  color: #838C95;
+  color: #838c95;
   padding: 9.5px;
 
   &:first-child {
-    border-right: 1px solid #838C95;
+    border-right: 1px solid #838c95;
     border-top-left-radius: 3px;
     border-bottom-left-radius: 3px;
   }
@@ -89,37 +89,37 @@ const SortButton = styled.div`
 `;
 
 const Main = () => {
-    const [totalNum, setTotalNum] = useState(0);
+  const [totalNum, setTotalNum] = useState(0);
 
-    fetch(`${process.env.REACT_APP_SERVER}/questions`)
-        .then((res) => {
-            return res.json();
-        })
-        .then((data) => {
-            setTotalNum(data.pageInfo.totalElements);
-        });
+  fetch(`${process.env.REACT_APP_SERVER}/questions`)
+    .then((res) => {
+      return res.json();
+    })
+    .then((data) => {
+      setTotalNum(data.pageInfo.totalElements);
+    });
 
-    return (
-        <Content id="content">
-            <Mainbar id="mainbar">
-                <Wrapper>
-                    <span className="question-title">All Questions</span>
-                    <Link to="/create">
-                        <AskButton>Ask Question</AskButton>
-                    </Link>
-                </Wrapper>
-                <Wrapper className='ai-center jc-space-between'>
-                    <h3>{totalNum} questions</h3>
-                    <div className='filter'>
-                        <SortButton>Newest</SortButton>
-                        <SortButton>Vote</SortButton>
-                    </div>
-                </Wrapper>
-                <QuestionList />
-            </Mainbar>
-            <SidebarRight />
-        </Content>
-    );
+  return (
+    <Content id="content">
+      <Mainbar id="mainbar">
+        <Wrapper>
+          <span className="question-title">All Questions</span>
+          <Link to="/create">
+            <AskButton>Ask Question</AskButton>
+          </Link>
+        </Wrapper>
+        <Wrapper className="ai-center jc-space-between">
+          <h3>{totalNum} questions</h3>
+          <div className="filter">
+            <SortButton>Newest</SortButton>
+            <SortButton>Vote</SortButton>
+          </div>
+        </Wrapper>
+        <QuestionList />
+      </Mainbar>
+      <SidebarRight />
+    </Content>
+  );
 };
 
 export default Main;
