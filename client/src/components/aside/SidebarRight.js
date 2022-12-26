9ff63fa27f0e9d53abff847ea4ceda9787f666e8
ashleysyheo <ashleysyheo@gmail.com>
2022-12-27 01:37:09 +0900

    refactor: change svg icon

--- client/src/components/aside/SidebarRight.js
+++ client/src/components/aside/SidebarRight.js
@@ -1,6 +1,6 @@
 import styled from 'styled-components';
-import * as Icons from "@stackoverflow/stacks-icons";
-import React from "react";
+import * as Icons from '@stackoverflow/stacks-icons';
+import React from 'react';
 
 const Sidebar = styled.div`
   float: right;
@@ -42,6 +42,10 @@ const Sidebar = styled.div`
 
       & svg {
         vertical-align: text-top;
+
+        & > * {
+          fill: #232629 !important;
+        }
       }
     }
 
@@ -53,86 +57,86 @@ const Sidebar = styled.div`
 `;
 
 export default function SidebarRight() {
-    return (
-        <Sidebar className="right-sidebar">
-            <ul>
-                <li className="sidebar--heading">The Overflow Blog</li>
-                <li className="sidebar--item">
-                    <div className="svg-icon">
-                        <svg
-                            xlink="http://www.w3.org/1999/xlink"
-                            xmlns="http://www.w3.org/2000/svg"
-                            aria-hidden="true"
-                            class="va-text-top svg-icon iconPencilSm"
-                            width="14"
-                            height="14"
-                            viewBox="0 0 14 14"
-                        >
-                            <path
-                                d="m11.1 1.71 1.13 1.12c.2.2.2.51 0 .71L11.1 4.7 9.21 2.86l1.17-1.15c.2-.2.51-.2.71 0ZM2 10.12l6.37-6.43 1.88 1.88L3.88 12H2v-1.88Z"
-                                fill="#232629"
-                            ></path>
-                        </svg>
-                    </div>
-                    <span>The complete guide to protecting your APIs with OAuth2 (part 1)</span>
-                </li>
-                <li className="sidebar--item">
-                    <div className="svg-icon">
-                        <svg
-                            xlink="http://www.w3.org/1999/xlink"
-                            xmlns="http://www.w3.org/2000/svg"
-                            aria-hidden="true"
-                            class="va-text-top svg-icon iconPencilSm"
-                            width="14"
-                            height="14"
-                            viewBox="0 0 14 14"
-                        >
-                            <path
-                                d="m11.1 1.71 1.13 1.12c.2.2.2.51 0 .71L11.1 4.7 9.21 2.86l1.17-1.15c.2-.2.51-.2.71 0ZM2 10.12l6.37-6.43 1.88 1.88L3.88 12H2v-1.88Z"
-                                fill="#232629"
-                            ></path>
-                        </svg>
-                    </div>
-                    <span>The three top-paying tech roles in 2022 and the skills you need to land them</span>
-                </li>
-                <li className="sidebar--heading border-top">The Overflow Blog</li>
-                <li className="sidebar--item">
-                    <div
-                        className="svg-icon"
-                        dangerouslySetInnerHTML={{
-                            __html: Icons.IconSpeechBubbleRightSm,
-                        }}
-                    />
-                    <span>Navigation and UI research starting soon</span>
-                </li>
-                <li className="sidebar--item">
-                    <div
-                        className="svg-icon"
-                        dangerouslySetInnerHTML={{
-                            __html: Icons.IconLogoGlyphXxs,
-                        }}
-                    />
-                    <span>2022 Community Moderator Election Results - now with two more mods!</span>
-                </li>
-                <li className="sidebar--item">
-                    <div
-                        className="svg-icon"
-                        dangerouslySetInnerHTML={{
-                            __html: Icons.IconLogoGlyphXxs,
-                        }}
-                    />
-                    <span>Temporary policy: ChatGPT is banned</span>
-                </li>
-                <li className="sidebar--item">
-                    <div
-                        className="svg-icon"
-                        dangerouslySetInnerHTML={{
-                            __html: Icons.IconLogoGlyphXxs,
-                        }}
-                    />
-                    <span>I'm standing down as a moderator</span>
-                </li>
-            </ul>
-        </Sidebar>
-    );
+  return (
+    <Sidebar className="right-sidebar">
+      <ul>
+        <li className="sidebar--heading">The Overflow Blog</li>
+        <li className="sidebar--item">
+          <div className="svg-icon">
+            <svg
+              xlink="http://www.w3.org/1999/xlink"
+              xmlns="http://www.w3.org/2000/svg"
+              aria-hidden="true"
+              class="va-text-top svg-icon iconPencilSm"
+              width="14"
+              height="14"
+              viewBox="0 0 14 14"
+            >
+              <path
+                d="m11.1 1.71 1.13 1.12c.2.2.2.51 0 .71L11.1 4.7 9.21 2.86l1.17-1.15c.2-.2.51-.2.71 0ZM2 10.12l6.37-6.43 1.88 1.88L3.88 12H2v-1.88Z"
+                fill="#232629"
+              ></path>
+            </svg>
+          </div>
+          <span>The complete guide to protecting your APIs with OAuth2 (part 1)</span>
+        </li>
+        <li className="sidebar--item">
+          <div className="svg-icon">
+            <svg
+              xlink="http://www.w3.org/1999/xlink"
+              xmlns="http://www.w3.org/2000/svg"
+              aria-hidden="true"
+              class="va-text-top svg-icon iconPencilSm"
+              width="14"
+              height="14"
+              viewBox="0 0 14 14"
+            >
+              <path
+                d="m11.1 1.71 1.13 1.12c.2.2.2.51 0 .71L11.1 4.7 9.21 2.86l1.17-1.15c.2-.2.51-.2.71 0ZM2 10.12l6.37-6.43 1.88 1.88L3.88 12H2v-1.88Z"
+                fill="#232629"
+              ></path>
+            </svg>
+          </div>
+          <span>The three top-paying tech roles in 2022 and the skills you need to land them</span>
+        </li>
+        <li className="sidebar--heading border-top">The Overflow Blog</li>
+        <li className="sidebar--item">
+          <div
+            className="svg-icon"
+            dangerouslySetInnerHTML={{
+              __html: Icons.IconLogoGlyphXxs,
+            }}
+          />
+          <span>Navigation and UI research starting soon</span>
+        </li>
+        <li className="sidebar--item">
+          <div
+            className="svg-icon"
+            dangerouslySetInnerHTML={{
+              __html: Icons.IconLogoGlyphXxs,
+            }}
+          />
+          <span>2022 Community Moderator Election Results - now with two more mods!</span>
+        </li>
+        <li className="sidebar--item">
+          <div
+            className="svg-icon"
+            dangerouslySetInnerHTML={{
+              __html: Icons.IconLogoGlyphXxs,
+            }}
+          />
+          <span>Temporary policy: ChatGPT is banned</span>
+        </li>
+        <li className="sidebar--item">
+          <div
+            className="svg-icon"
+            dangerouslySetInnerHTML={{
+              __html: Icons.IconLogoGlyphXxs,
+            }}
+          />
+          <span>I'm standing down as a moderator</span>
+        </li>
+      </ul>
+    </Sidebar>
+  );
 }
