2bff7e07fbd8fb16b6beb24bcc840a2b68903a50
hyejinme <kimhyejin703@daum.net>
2022-12-26 15:35:16 +0900

    ✨feat: 질문, 답변 추천-비추천 기능 추가

---
+++ server/stackoverflow/src/main/java/com/y2k/stackoverflow/question/entity/QuestionVote.java
@@ -0,0 +1,32 @@
+package com.y2k.stackoverflow.question.entity;
+
+import com.y2k.stackoverflow.member.entity.Member;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@Entity
+public class QuestionVote {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long voteId;
+
+    @ManyToOne
+    @JoinColumn(name = "member_id")
+    private Member member;
+    
+    @ManyToOne
+    @JoinColumn(name = "question_id")
+    private Question question;
+
+    public void setMember(Member member) {
+        this.member = member;
+    }
+
+    public void setQuestion(Question question) {
+        this.question = question;
+    }
+}
