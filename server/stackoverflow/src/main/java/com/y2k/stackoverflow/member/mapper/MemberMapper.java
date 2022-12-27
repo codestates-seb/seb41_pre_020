779679127c419dac1f324fe5e2b63c00c37ea7cc
kim hyejin <97475429+hyejinme@users.noreply.github.com>
2022-12-27 12:44:41 +0900

    [BE] 회원, 댓글 시간 응답 데이터 수정 (#56)
    * ✨feat: 질문 관련 엔티티, dto 추가
    
    * ✨feat: 질문 등록, 수정, 삭제, 조회 기능 추가
    
    * ✨feat: 공통 기능 변경
    
    * ✨feat: 질문 Dto 유효성 검증 추가
    
    * etc: Tag entity 삭제 - questionTag 통합
    
    * ✨feat: 질문 조회수 기능 추가, 예외처리
    
    * ✨feat: 질문 조회 시 답변 함께 출력하는 기능 추가
    
    * ✨feat: 답변 관련 엔티티, dto 추가
    
    * ✨feat: 답변 등록, 수정, 삭제, 조회 기능 추가
    
    * 🐛fix: 질문 수정 시 태그 중복 등록 수정
    
    * ✨feat: ExceptionCode 추가
    
    * ✨feat: 회원 비밀번호 수정 기능 , 로그인한 회원 조회 기능 추가
    
    * ✨feat: 응답 데이터 수정
    
    * ✨feat: 질문, 답변, 태그 추가 매핑
    
    * ✨feat: 질문, 답변 추천-비추천 기능 추가
    
    * ♻️refactor: 질문, 답변 추천-비추천 기능 로직 변경
    
    * ✨feat: LocalDateTime Custom 변경 파일 추가
    
    * ✨feat: 채택 기능 추가
    
    * 🐛fix: 질문 수정 시 update문 두번 실행 되던 부분 수정
    
    * 🐛fix: 질문 전체 조회 시 삭제 처리된 태그가 보이는 부분 수정
    
    * ✨feat: 질문, 답변 응답 데이터 시간 표시 수정
    
    * ✨feat: 댓글 응답 데이터 시간 표시 수정
    
    * ✨feat: 회원 응답 데이터 시간 표시 수정

--- server/stackoverflow/src/main/java/com/y2k/stackoverflow/member/mapper/MemberMapper.java
+++ server/stackoverflow/src/main/java/com/y2k/stackoverflow/member/mapper/MemberMapper.java
@@ -2,6 +2,7 @@ package com.y2k.stackoverflow.member.mapper;
 
 import com.y2k.stackoverflow.member.dto.MemberDto;
 import com.y2k.stackoverflow.member.entity.Member;
+import com.y2k.stackoverflow.util.DateUtil;
 import org.mapstruct.Mapper;
 
 import java.util.List;
@@ -15,7 +16,19 @@ public interface MemberMapper {
 
     MemberDto.Response memberToResponseDto(Member member);
 
-    MemberDto.DetailsResponse memberToDetailsResponseDto(Member member);
+    //MemberDto.DetailsResponse memberToDetailsResponseDto(Member member);
+    default MemberDto.DetailsResponse memberToDetailsResponseDto(Member member) {
+        MemberDto.DetailsResponse detailsResponse = new MemberDto.DetailsResponse();
+        detailsResponse.setMemberId(member.getMemberId());
+        detailsResponse.setDisplayName(member.getDisplayName());
+        detailsResponse.setProfileImage(member.getProfileImage());
+        detailsResponse.setMemberStatus(member.getMemberStatus());
+        detailsResponse.setCreatedAt(DateUtil.convertLocalDatetimeToTime(member.getCreatedAt()));
+        detailsResponse.setModifiedAt(DateUtil.convertLocalDatetimeToTime(member.getModifiedAt()));
+        detailsResponse.setUserProfile(member.getUserProfile());
+
+        return detailsResponse;
+    }
     List<MemberDto.Response> membersToResponseDto(List<Member> members);
 
 }
