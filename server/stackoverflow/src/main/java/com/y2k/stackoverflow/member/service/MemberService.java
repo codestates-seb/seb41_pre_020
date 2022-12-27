79c97bc82c5cad4bb5fda979c7f2b5770da8a4d2
kimmj13 <110887976+kimmj13@users.noreply.github.com>
2022-12-27 10:28:08 +0900

    [BE] 회원 정보 수정 에러 처리 (#54)
    * 공통기능 추가 - dto, exception, audit
    
    * :sparkles: feat: 회원 컨트롤러 추가
    
    회원 등록, 회원 수정, 회원(1인/전체) 조회, 회원 삭제
    
    * :sparkles: feat: 회원 리포지토리 추가
    
    * ✨feat: 회원 가입, 수정, 삭제, 조회 기능 추가
    
    * apply .gitignore
    
    * Update issue templates
    
    * :sparkles: feat: 유효성 검증 로직 변경
    
    * ✨feat: 회원 비밀번호 암호화 기능 추가, 회원 전체 조회 기능 보완
    
    * ✨feat: 로그인/로그아웃 JWT 기능 추가
    
    * ✨feat: 보안관련 설정파일 변경
    
    * ✨feat: 네이밍 변경
    
    * 보안추가로 주석 해제
    
    * 보안 기능 추가
    
    * ♻️refactor: url, 및 마이페이지 profile 수정 기능 추가
    
    * ✨feat: 보안 기능 추가
    
    * ♻️조회 권한 모두 허용
    
    * ✨feat: 소셜 로그인 기능 추가 및 소셜로그인시 pw 변경 불가 로직 추가
    
    * ✨🐛fix: 소셜로그인시 pw 변경 불가 기능 오류 수정
    
    * :bug: fix: 일반 회원 가입 회원 pw 오류 처리

--- server/stackoverflow/src/main/java/com/y2k/stackoverflow/member/service/MemberService.java
+++ server/stackoverflow/src/main/java/com/y2k/stackoverflow/member/service/MemberService.java
@@ -6,7 +6,6 @@ import com.y2k.stackoverflow.exception.ExceptionCode;
 import com.y2k.stackoverflow.member.entity.Member;
 import com.y2k.stackoverflow.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
@@ -18,6 +17,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.regex.Pattern;
 
 @Service
 @Transactional
@@ -25,8 +25,6 @@ import java.util.Optional;
 public class MemberService {
 
     private final MemberRepository memberRepository;
-    private final ApplicationEventPublisher publisher;
-
     private final PasswordEncoder passwordEncoder;
     private final CustomAuthorityUtils authorityUtils;
 
@@ -53,12 +51,19 @@ public class MemberService {
         Optional.ofNullable(member.getUserProfile())
                 .ifPresent(findMember::setUserProfile);
 
-        //소셜로그인 회원인 경우 pw 수정 불가
-        if (findMember.getPassword() == null) {
+        if (findMember.getPassword() == null && !member.getPassword().isEmpty()) {
             throw new BusinessLogicException(ExceptionCode.ACCESS_FORBIDDEN);
         }
+
+        String pattern = "(?=.*[0-9])(?=.*[a-z])(?=.*\\W)(?=\\S+$).{8,16}";
+        String password = member.getPassword();
+
+        if (member.getPassword() != null && !Pattern.matches(pattern, password)) {
+            throw new BusinessLogicException(ExceptionCode.INVALID_PASSWORD);
+        }
+
         Optional.ofNullable(member.getPassword())
-                .ifPresent(password -> findMember.setPassword(password));
+                .ifPresent(findMember::setPassword);
 
         return (Member) memberRepository.save(findMember);
     }
