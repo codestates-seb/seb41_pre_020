167f74a7a9ad166c732f2ce9c935c427f168c702
kim hyejin <97475429+hyejinme@users.noreply.github.com>
2022-12-28 09:28:04 +0900

    [BE] 질문 검색 기능 추가 및 수정 (#73)
    * ✨feat: 질문 관련 엔티티, dto 추가
    
    * ✨feat: 질문 등록, 수정, 삭제, 조회 기능 추가
    
    * ✨feat: 공통 기능 변경
    
    * Update issue templates
    
    * ✨feat: 질문 Dto 유효성 검증 추가
    
    * etc: Tag entity 삭제 - questionTag 통합
    
    * ✨feat: 질문 조회수 기능 추가, 예외처리
    
    * ✨feat: 질문 조회 시 답변 함께 출력하는 기능 추가
    
    * ✨feat: 답변 관련 엔티티, dto 추가
    
    * ✨feat: 답변 등록, 수정, 삭제, 조회 기능 추가
    
    * 🐛fix: 질문 수정 시 태그 중복 등록 수정
    
    * ✨feat: ExceptionCode 추가
    
    * ✨feat: 회원 비밀번호 수정 기능 , 로그인한 회원 조회 기능 추가
    
    * ✨feat: 응답 데이터 수정
    
    * ✨feat: 질문, 답변, 태그 추가 매핑
    
    * ✨feat: 질문, 답변 추천-비추천 기능 추가
    
    * ♻️refactor: 질문, 답변 추천-비추천 기능 로직 변경
    
    * ✨feat: LocalDateTime Custom 변경 파일 추가
    
    * ✨feat: 채택 기능 추가
    
    * 🐛fix: 질문 수정 시 update문 두번 실행 되던 부분 수정
    
    * 🐛fix: 질문 전체 조회 시 삭제 처리된 태그가 보이는 부분 수정
    
    * ✨feat: 질문, 답변 응답 데이터 시간 표시 수정
    
    * ✨feat: 댓글 응답 데이터 시간 표시 수정
    
    * ✨feat: 회원 응답 데이터 시간 표시 수정
    
    * 🐛fix: 전체 질문 조회 시 questionCheck null값 수정
    
    * ✨feat: 추천순, 최신순 정렬 기능 추가 및 추천 기능 수정
    
    * 🐛fix: 채택 기능 접근 권한 수정
    
    * ✨feat: 질문 검색 기능 추가
    
    * 🐛fix: 질문 조회수 올라가는 부분 서비스 로직 분리
    
    * 🐛fix: 질문 조회수 올라가는 부분 서비스 로직 분리
    
    * 🐛fix: 사용하지 않는 응답 데이터 삭제
    
    Co-authored-by: kimmj13 <110887976+kimmj13@users.noreply.github.com>

--- server/stackoverflow/src/main/java/com/y2k/stackoverflow/question/repository/QuestionRepository.java
+++ server/stackoverflow/src/main/java/com/y2k/stackoverflow/question/repository/QuestionRepository.java
@@ -1,7 +1,12 @@
 package com.y2k.stackoverflow.question.repository;
 
 import com.y2k.stackoverflow.question.entity.Question;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface QuestionRepository extends JpaRepository<Question, Long> {
+    @Query("SELECT q FROM Question q where q.title LIKE %:search% OR q.content LIKE %:search%")
+    Page<Question> searchQuestion(PageRequest pageRequest, String search);
 }
