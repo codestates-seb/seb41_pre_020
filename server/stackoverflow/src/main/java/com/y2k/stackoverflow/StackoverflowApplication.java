60ff1f8464e5de2251aadd4ac8c594d01de237e2
kimmj13 <qlckadk@gmail.com>
2022-12-30 13:48:50 +0900

    :recycle: refactor: 변수 설정 수정

--- server/stackoverflow/src/main/java/com/y2k/stackoverflow/StackoverflowApplication.java
+++ server/stackoverflow/src/main/java/com/y2k/stackoverflow/StackoverflowApplication.java
@@ -7,7 +7,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
-@PropertySource("classpath:/environ/env.properties")
+@PropertySource("classpath:/env.properties")
 public class StackoverflowApplication {
 
 	public static void main(String[] args) {
