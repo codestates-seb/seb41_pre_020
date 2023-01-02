d255745a270e39476df049f6b2e7005d4d595b58
kimmj13 <qlckadk@gmail.com>
2023-01-02 12:23:12 +0900

    로그아웃 설정 변경

--- server/stackoverflow/src/main/java/com/y2k/stackoverflow/auth/refresh/LogoutController.java
+++ server/stackoverflow/src/main/java/com/y2k/stackoverflow/auth/refresh/LogoutController.java
@@ -6,13 +6,14 @@ import com.y2k.stackoverflow.exception.BusinessLogicException;
 import com.y2k.stackoverflow.exception.ExceptionCode;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.params.shadow.com.univocity.parsers.annotations.Headers;
 import org.springframework.dao.InvalidDataAccessApiUsageException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Map;
 
 @RestController
 @RequiredArgsConstructor
@@ -21,27 +22,19 @@ import org.springframework.web.bind.annotation.RestController;
 public class LogoutController {
     private final JwtTokenizer jwtTokenizer;
     private final RedisUtil redisUtil;
-    private final TokenRepository tokenRepository;
+//    private final TokenRepository tokenRepository;
 
-    @PostMapping("/logout")
-    public ResponseEntity logout(@RequestBody LogoutDto logoutDto) {
+    @PostMapping(path = "/logout", headers = "Authorization")
+    public ResponseEntity logout(@RequestHeader String authorization) {
 
-        String refreshToken = logoutDto.getRefreshToken();
-        String accessToken = logoutDto.getAccessToken();
         String key = jwtTokenizer.encodeBase64SecretKey(jwtTokenizer.getSecretKey());
-
+        String accessToken = authorization.replace("Bearer ", "");
         // Access Token 검증
         try {
             if (!jwtTokenizer.validateToken(accessToken, key)) {
                 throw new BusinessLogicException(ExceptionCode.INVALID_TOKEN_STATUS);
             }
         } catch (NullPointerException ne) {
-            log.info(ne.getMessage());
-        }
-        try {
-            Token findRefreshToken = tokenRepository.findByRefreshToken(refreshToken);
-            tokenRepository.delete(findRefreshToken);
-        } catch (InvalidDataAccessApiUsageException ne) {
             log.info(ne.getMessage());
         }
 //             DB에 저장된 Refresh Token 제거,
