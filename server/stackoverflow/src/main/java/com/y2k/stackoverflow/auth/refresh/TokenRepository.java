15bfd62bd9b9cb16d0e029f70636e9a0f32c5537
kimmj13 <qlckadk@gmail.com>
2023-01-02 12:41:34 +0900

    파일 정리

--- server/stackoverflow/src/main/java/com/y2k/stackoverflow/auth/refresh/TokenRepository.java
+++
@@ -1,7 +0,0 @@
-package com.y2k.stackoverflow.auth.refresh;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TokenRepository extends JpaRepository<Token, Long> {
-    Token findByRefreshToken(String refreshToken);
-}
